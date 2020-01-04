30f974c79cea9d629921c45e7ccf61a8a386ee6d
Ying Lu <yinglu91@gmail.com>
2020-01-04 14:38:51 -0500

    added genres

--- server/src/datasources/artist.js
+++ server/src/datasources/artist.js
@@ -1,49 +1,45 @@
-const {
-    RESTDataSource
-} = require('apollo-datasource-rest');
+const { RESTDataSource } = require('apollo-datasource-rest');
 
 class ArtistAPI extends RESTDataSource {
-    constructor() {
-        super();
-        this.baseURL = 'https://spotify-api-wrapper.appspot.com/';
-    }
+  constructor() {
+    super();
+    this.baseURL = 'https://spotify-api-wrapper.appspot.com/';
+  }
 
-    // https://spotify-api-wrapper.appspot.com/artist/Whitney Houston
-    async getArtistByName({
-        artistName
-    }) {
-        const response = await this.get(`artist/${artistName}`);
-        console.log(response);
-        return this.artistReducer(response.artists.items[0]);
-    }
+  // https://spotify-api-wrapper.appspot.com/artist/Whitney Houston
+  async getArtistByName({ artistName }) {
+    const response = await this.get(`artist/${artistName}`);
+    console.log(response);
+    return this.artistReducer(response.artists.items[0]);
+  }
 
-    artistReducer(artist) {
-        return {
-            id: artist.id,
-            name: artist.name,
-            image: artist.images[1].url,
-            followers: artist.followers.total
-        };
-    }
+  artistReducer(artist) {
+    return {
+      id: artist.id,
+      name: artist.name,
+      imageUrl: artist.images[1].url,
+      genres: artist.genres,
+      followers: artist.followers.total
+    };
+  }
 
-    // https://spotify-api-wrapper.appspot.com/artist/6XpaIBNiVzIetEPCWDvAFP/top-tracks
-    async getTracksByArtistId({
-        artistId
-    }) {
-        const response = await this.get(`artist/${artistId}/top-tracks`);
-        console.log(response);
-        return Array.isArray(response.tracks) ?
-            response.tracks.map(track => this.trackReducer(track)) : [];
+  // https://spotify-api-wrapper.appspot.com/artist/6XpaIBNiVzIetEPCWDvAFP/top-tracks
+  async getTracksByArtistId({ artistId }) {
+    const response = await this.get(`artist/${artistId}/top-tracks`);
+    console.log(response);
+    return Array.isArray(response.tracks)
+      ? response.tracks.map(track => this.trackReducer(track))
+      : [];
+  }
 
-    }
-
-    trackReducer(track) {
-        return {
-            name: track.name,
-            image: track.album.images[0].url,
-            previewUrl: track.preview_url
-        };
-    }
+  trackReducer(track) {
+    return {
+      id: track.album.id,
+      name: track.name,
+      imageUrl: track.album.images[0].url,
+      previewUrl: track.preview_url
+    };
+  }
 }
 
 module.exports = ArtistAPI;
